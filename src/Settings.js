4e1aeeedb03a941990660eadf62c21e560ddd802
nikolajkring <80462494+nikolajkring@users.noreply.github.com>
2022-11-16 16:29:16 +0100

    Lavet settings næsten done

--- src/Settings.js
+++ src/Settings.js
@@ -1,59 +1,89 @@
 import './Settings.css';
-
-
+import avatar from "./img/avatar.jpg";
+import house from "./img/house.jpg";
 
 
 function settings() {
     return (
         <>
 
-            <div class="grid-container">
+            {/* ACCOUNT */}
 
-                {/* Sprog */}
+            <div class="avatar grid-container-3">
                 <div class="grid-item">
-                    <h2>Sprog</h2>
-                    <h3>Dansk / Engelsk</h3>
+                    <img src={avatar} alt="avatar" height="50" />
+                    <h3>Jeppe Lorentzen</h3>
                 </div>
 
-                <label class="switch grid-item">
-                    <input type="checkbox" />
-                    <span class="slider round labels"></span>
-                </label>
-
-                {/* Applikationlyde */}
                 <div class="grid-item">
-                    <h2>Applikationlyde</h2>
-                    <h3>xxx</h3>
+                    <img src={house} alt="house logo" height="125px" />
                 </div>
 
-                <label class="switch grid-item">
-                    <input type="checkbox" />
-                    <span class="slider round labels"></span>
-                </label>
-
-                {/* E-mail-kvittering */}
                 <div class="grid-item">
-                    <h2>E-mail-kvittering</h2>
-                    <h3>xxx</h3>
+                    <h1>Indstillinger</h1>
                 </div>
 
-                <label class="switch grid-item">
-                    <input type="checkbox" />
-                    <span class="slider round labels"></span>
-                </label>
 
-                {/* Dark mode */}
-                <div class="grid-item">
-                    <h2>Dark mode</h2>
-                    <h3>xxx</h3>
-                </div>
-                <label class="switch grid-item">
-                    <input type="checkbox" />
-                    <span class="slider round labels"></span>
-                </label>
             </div>
 
 
+            <div class="hjem-bg wrapper-1">
+                <div class="grid-container-2 bg-white wrapper-2">
+
+
+                    {/* Sprog */}
+                    <div class="grid-item">
+                        <h2>Sprog</h2>
+                        <h3>Dansk / Engelsk</h3>
+                    </div>
+
+                    <label class="switch grid-item">
+                        <input type="checkbox" />
+                        <span class="slider round labels"></span>
+                    </label>
+
+                    {/* Applikationlyde */}
+                    <div class="grid-item">
+                        <h2>Applikationlyde</h2>
+                        <h3>xxx</h3>
+                    </div>
+
+                    <label class="switch grid-item">
+                        <input type="checkbox" />
+                        <span class="slider round labels"></span>
+                    </label>
+
+                    {/* E-mail-kvittering */}
+                    <div class="grid-item">
+                        <h2>E-mail-kvittering</h2>
+                        <h3>xxx</h3>
+                    </div>
+
+                    <label class="switch grid-item">
+                        <input type="checkbox" />
+                        <span class="slider round labels"></span>
+                    </label>
+
+                    {/* Dark mode */}
+                    <div class="grid-item">
+                        <h2>Dark mode</h2>
+                        <h3>xxx</h3>
+                    </div>
+                    <label class="switch grid-item">
+                        <input type="checkbox" />
+                        <span class="slider round labels"></span>
+                    </label>
+
+
+                </div>
+                {/* Hjem knap */}
+                <button class="hjem-knap center">Hjem</button>
+
+            </div>
+
+
+            {/* copy right */}
+            <p class="center">copyright © Bookin</p>
         </>
     )
 }
