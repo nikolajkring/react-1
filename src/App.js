91f6ab81ae0a07e5fce29608a04a6b8311ab4587
nikolajkring <80462494+nikolajkring@users.noreply.github.com>
2022-11-16 12:49:37 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import './App.css';
+import { useState, useEffect } from "react";
+// import { getSuggestedQuery } from '@testing-library/react';
+import { db } from "./firebase-config";
+import { collection, getDocs, deleteDoc, doc } from 'firebase/firestore';
+import { async } from '@firebase/util';
+import furniture from "./img/furniture.jpg";
+import light from "./img/light.jpg";
+import projector from "./img/projector.jpg";
+import size from "./img/size.jpg";
+
+
+
+
+
+function App() {
+  const [rooms, setRooms] = useState([]);
+  const roomsColRef = collection(db, "lokaler")
+
+
+  const deleteBooking = async (id) => {
+    const roomDoc = doc(db, "lokaler", id);
+    await deleteDoc(roomDoc);
+  }
+
+  
+
+  useEffect(() => {
+    const getRooms = async () => {
+      const data = await getDocs(roomsColRef);
+      setRooms(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+
+    getRooms();
+  }, []
+  );
+
+  return <div className='App'>{rooms.map((room) => {
+    return (
+      <div>
+        <h1>{room.number}</h1>
+        <h1>{room.size}
+        {<img src={size} alt="size" height="25" />}
+        </h1>
+        {rooms[0].light && <img src={light} alt="light" height="25" />}
+        {rooms[0].projector && <img src={projector} alt="projector" height="25" />}
+        {rooms[0].furniture && <img src={furniture} alt="furniture" height="25" />}
+        <button>Edit</button>
+        <button onClick={() => { deleteBooking(room.id) }}>Deleted</button>
+      </div>
+    );
+  })}</div>
+}
+
+
+
+// function SlideKnap() {
+//   return (
+//     <>
+//       <label class="switch">
+//         <input type="checkbox" />
+//         <span class="slider round"></span>
+//       </label>
+//     </>)
+// }
+
+export default App;
