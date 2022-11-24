9ee03855edcccb945664081210453ed9360e4302
nikolajkring <80462494+nikolajkring@users.noreply.github.com>
2022-11-24 18:53:41 +0100

    Updates

--- src/SingleBooking.js
+++
@@ -1,8 +0,0 @@
-export default function SingleBooking({ date, room }) {
-    return (
-      <div>
-        <h1>{room}</h1>
-        <p>{date}</p>
-      </div>
-    );
-  }
--- src/Spinner.js
+++
@@ -1,5 +0,0 @@
-
-export function Spinner() {
-    return (
-    <div className="lds-dual-ring"></div>
-    ); }
